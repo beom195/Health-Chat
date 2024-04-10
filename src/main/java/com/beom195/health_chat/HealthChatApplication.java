43c0b0d1de807bc1b6265e4cdbdac8547657a94d
beom195 <realcriminal195@gmail.com>
2024-04-10 16:55:18 +0900

    commit test

---
+++ src/main/java/com/beom195/health_chat/HealthChatApplication.java
@@ -0,0 +1,13 @@
+package com.beom195.health_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthChatApplication.class, args);
+    }
+
+}
